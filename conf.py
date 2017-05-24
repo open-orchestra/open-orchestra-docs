a9295cdd240d7059f2ae3bac611c61afde002c68
Amaury Lavieille <amaury.lavieille@businessdecision.com>
2017-05-24 09:53:55 +0200

    fix ignore doc

--- conf.py
+++ conf.py
@@ -85,7 +85,7 @@ language = 'fr'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['en/*']
+exclude_patterns = ['old_doc/*']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
